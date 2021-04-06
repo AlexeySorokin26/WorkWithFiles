f9ae6781b0c3ebbdf050fa25a6cda1d4c86d9951
Alexey Sorokin <asorokin@ukaachen.de>
2021-04-06 18:18:51 +0200

    first task check if files and folders was not modified more than 30 minutes then delete them

--- WorkWithFiles1/Program.cs
+++ WorkWithFiles1/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.Threading;
+using System.IO;
 
 namespace WorkWithFiles1
 {
+    
     class Program
     {
+        const int minutesToCleanUp = 10;
+        public static bool CheckModificationFileTime(FileInfo f)
+        {
+            DateTime modificationTime = f.LastWriteTime;
+            DateTime nowTime = DateTime.Now;
+            if (nowTime - modificationTime > TimeSpan.FromMinutes(minutesToCleanUp))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CheckModificationDirTime(DirectoryInfo d)
+        {
+            DateTime modificationTime = d.LastWriteTime;
+            DateTime nowTime = DateTime.Now;
+            if (nowTime - modificationTime > TimeSpan.FromSeconds(minutesToCleanUp))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void CleanUpFolder(DirectoryInfo dir)
+        {
+            FileInfo[] files = dir.GetFiles();
+            foreach(FileInfo f in files)
+            {
+                if (f.Exists && CheckModificationFileTime(f))
+                {
+                    f.Delete();                    
+                }
+            }
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo d in dirs)
+            {       
+                if (d.Exists)
+                {
+                    CleanUpFolder(d);
+                    if (CheckModificationDirTime(d))
+                    {
+                        d.Delete();
+                    }
+                    
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"D:\Learning\C++\code\SkillFactory\c#\WorkWithFiles1\WorkWithFiles1\tmp";
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                CleanUpFolder(dir);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
         }
     }
 }
--- WorkWithFiles4/Program.cs
+++ WorkWithFiles4/Program.cs
@@ -4,9 +4,12 @@ namespace WorkWithFiles4
 {
     class Program
     {
+
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
